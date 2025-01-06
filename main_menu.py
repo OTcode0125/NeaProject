df2979bc09191034441e0ac0cfab16c690e15693
OTcode0125 <oliver.jtomlinson>
2025-01-06 21:05:43 +0000

    changes to main menu

--- main_menu.py
+++ main_menu.py
@@ -1,15 +1,20 @@
 import pygame
 
 def main_menu_screen(display):
-    display.fill(pygame.Color("lightblue"))
+    display.fill(pygame.Color("grey"))
 
-    font = pygame.font.SysFont("Roman", 70)
-    title_surface = font.render("fARMSWEEPER", True, (0, 0, 0))
-    display.blit(title_surface, (400, 100))
+    big_font = pygame.font.SysFont("Roman", 70)
+    small_font = pygame.font.SysFont("Roman", 36)
+    instruction_font = pygame.font.SysFont("Arial", 30)
 
-    instruction_font = pygame.font.SysFont("Arial", 50)
-    instruction_text = instruction_font.render("Press Enter to Start the Game", True, (0, 0, 0))
-    display.blit(instruction_text, (500, 400))
+    title_surface = big_font.render("FARMSWEEPER", True, (0, 0, 0))
+    display.blit(title_surface, (10,10))
+
+    sub_title_surface = small_font.render("Discover all the cells free of the moles to win the game!", True, (0,0,0))
+    display.blit(sub_title_surface, (10,100))
+
+    instruction_text = instruction_font.render("PRESS (ENTER) TO PLAY GAME AS GUEST", True, (0, 0, 0))
+    display.blit(instruction_text, (650, 1000))
 
     '''keys = pygame.key.get_pressed()
     if keys[pygame.K_RETURN]:
