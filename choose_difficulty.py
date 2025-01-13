9d874670c420fdecf613249b7f9f49b6c541c4d7
OTcode0125 <oliver.jtomlinson>
2025-01-13 21:12:43 +0000

    deleting some stuff

--- choose_difficulty.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-def choose_difficulty_screen(display):
-    display.fill(pygame.Color("grey"))
-
-    big_font = pygame.font.SysFont("Roman", 36)
-    
-    title_surface = big_font.render("Difficulty:", True, (0, 0, 0))
-    display.blit(title_surface, (10, 10))
--- login.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-def login_screen(display):
-    display.fill(pygame.Color("grey"))
-
-    big_font = pygame.font.SysFont("Roman", 36)
-    
-    title_surface = big_font.render("Login:", True, (0, 0, 0))
-    display.blit(title_surface, (10, 10))
